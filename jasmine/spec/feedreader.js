fce3b311b9297060dde4edb902241ba803653dd5
Brian Walton <bwalton836@gmail.com>
2016-12-13 20:32:21 -0500

    Updates from review 2

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -15,10 +15,7 @@ $(function() {
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -26,7 +23,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +35,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -52,21 +49,21 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
         it('Menu is hidden by default', function() {
             //  http://stackoverflow.com/questions/21987596/get-css-transform-property-with-jquery
-            var x = parseInt($('div.slide-menu').css('transform').split(',')[4]);
+            var x = parseInt($('.slide-menu').css('transform').split(',')[4]);
             expect(x).toBeLessThan(0);
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -84,7 +81,7 @@ $(function() {
 
             // After click and a delay. Transition value should be updated now.
             it('Menu shows on first menu click', function() {
-                var x = parseInt($('div.slide-menu').css('transform').split(',')[4]);
+                var x = parseInt($('.slide-menu').css('transform').split(',')[4]);
                 expect(x).toBeDefined();
                 // Check that menu is on-screen
                 expect(x).toEqual(0);
@@ -92,7 +89,7 @@ $(function() {
 
             // Another click and a delay. Transition values should be updated again.
             it('Menu does not show on next menu click', function() {
-                var x = parseInt($('div.slide-menu').css('transform').split(',')[4]);
+                var x = parseInt($('.slide-menu').css('transform').split(',')[4]);
                 expect(x).toBeDefined();
                 // Check that menu is off-screen
                 expect(x).toBeLessThan(0);
@@ -100,9 +97,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -110,41 +107,35 @@ $(function() {
          */
         beforeEach(function(done) {
             // Empty the current entries from the container
-            $('div.feed').empty();
+            $('.feed').empty();
             loadFeed(1, done);
         });
 
         it('loadFeed adds at least one entry to feed container', function() {
             // Look for an element with an .entry class.
-            var newEntries = $('div.feed .entry');
+            var newEntries = $('.feed .entry');
             // console.log('ne=',newEntries);
-            expect(newEntries).toBeDefined();
             expect(newEntries.length).toBeGreaterThan(0);
         });
 
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
     describe('New Feed', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
         var beforeHref;
         beforeEach(function(done) {
             // Start with a clean loadFeed.
-            $('div.feed').empty();
+            $('.feed').empty();
             loadFeed(2, function() {
                // Copy the link from the first element in the new feed list
                // then load another feed and check for change.
-               beforeHref = $('div.feed a:first').attr('href');
-               if (typeof beforeHref === 'undefined') {
-                  console.log('New Feed: loadFeed() failure');
-                  done();
-               } else {
-                  loadFeed(3, done);
-               }
+               beforeHref = $('.feed a:first').attr('href');
+               loadFeed(3, done);
             });
         });
 
@@ -153,7 +144,7 @@ $(function() {
             expect(beforeHref).toBeDefined();
             expect(beforeHref.length).toBeGreaterThan(0);
             // Check the second link that was collected. They should be different
-            var afterHref = $('div.feed a:first').attr('href');
+            var afterHref = $('.feed a:first').attr('href');
             // console.log('before=',beforeHref,'after=',afterHref);
             expect(afterHref).toBeDefined();
             expect(afterHref.length).toBeGreaterThan(0);
